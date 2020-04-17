41a09996954fc436deaf891c0f06846ff972ad23
Davide Proment <d.proment@uea.ac.uk>
2020-04-17 16:10:06 +0100

    data parsed

--- parseData.py
+++ parseData.py
@@ -10,7 +10,8 @@ import getDate
 
 
 
-url='https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2020/04/COVID-19-total-announced-deaths-' + getDate.getDateExcel() + '.xlsx'
+#url='https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2020/04/COVID-19-total-announced-deaths-' + getDate.getDateExcel() + '.xlsx'
+url='https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2020/04/COVID-19-all-announced-deaths-' + getDate.getDateExcel() + '.xlsx'
 print(url)
 
 #url='https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2020/04/COVID-19-total-announced-deaths-8-April-2020.xlsx'
